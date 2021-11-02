56018ecdf7de1a9f457ff8986230baf40cb8523d
cley1 <cley1@angelo.edu>
2021-11-02 17:32:20 -0500

    add const to bool

--- stage0.cpp
+++ stage0.cpp
@@ -261,7 +261,7 @@ string Compiler::ids() //token should be NON_KEY_ID - Z
 }
 
 // ---------------------------------------------------------------------------------
-bool Compiler::isKeyword(string s) // determines if s is a keyword
+bool Compiler::isKeyword(string s) const // determines if s is a keyword
 {
   if (s == "program" || s == "const" || s == "var" || s == "integer" || s == "boolean" || s == "begin" || s == "end" || s == "true" || s == "false" || s == "not")
   {
@@ -275,7 +275,7 @@ bool Compiler::isKeyword(string s) // determines if s is a keyword
 
 // ---------------------------------------------------------------------------------
 
-bool Compiler::isSpecialSymbol(char c) const; // determines if c is a special symbol
+bool Compiler::isSpecialSymbol(char c) const // determines if c is a special symbol
 {
   if (c == ':' || c == ',' || c == ';' || c == '=' || c == '+' || c == '-' || c == '.')
   {
@@ -289,7 +289,7 @@ bool Compiler::isSpecialSymbol(char c) const; // determines if c is a special sy
 
 // ---------------------------------------------------------------------------------
 
-bool Compiler::isNonKeyId(string s) const; // determines if s is a non_key_id
+bool Compiler::isNonKeyId(string s) const // determines if s is a non_key_id
 {
   if(!isKeyword(s))
   {
@@ -303,7 +303,7 @@ bool Compiler::isNonKeyId(string s) const; // determines if s is a non_key_id
 
 // ---------------------------------------------------------------------------------
 
-bool Compiler::isInteger(string s) const; // determines if s is an integer
+bool Compiler::isInteger(string s) const // determines if s is an integer
 {
     if(s == "0" || s == "1" || s == "2" || s == "3" || s == "4" || s == "5" || s == "6" || s == "7" || s == "8" || s == "9")
     {
@@ -317,7 +317,7 @@ bool Compiler::isInteger(string s) const; // determines if s is an integer
 
 // ---------------------------------------------------------------------------------
 
-bool Compiler::isBoolean(string s) const; // determines if s is a boolean - Cam
+bool Compiler::isBoolean(string s) const // determines if s is a boolean - Cam
 {
   if (s == "true" || s == "false")
   {
@@ -331,7 +331,7 @@ bool Compiler::isBoolean(string s) const; // determines if s is a boolean - Cam
 
 // ---------------------------------------------------------------------------------
 
-bool Compiler::isLiteral(string s) const; // determines if s is a literal
+bool Compiler::isLiteral(string s) const // determines if s is a literal
 {
 
 }
