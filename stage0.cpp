88d756ce99a06b55812f546317a773efd52914e3
cley1 <cley1@angelo.edu>
2021-11-24 10:36:17 -0600

    Idk?

--- stage0.cpp
+++ stage0.cpp
@@ -1787,7 +1787,7 @@ void Compiler::emitStorage()    //for those entries in the symbolTable that have
 		emit("", "mov", "eax,", "[" + symbolTable.at(operand2).getInternalName() + "]")
 		emit("", "idiv", "eax,[" + symbolTable.at(operand1).getInternalName() + "]", ";A Register = " + operand2 + "/" + operand1);	//not sure if this is right. we will need to add a comment here as well
 		emit("", "xchg", "eax,edx", "; grab remainder and put into eax");
-		
+
 		//deassign all temporaries involved and free those names for reuse
 		if (isTemporary(operand1))
 		{
