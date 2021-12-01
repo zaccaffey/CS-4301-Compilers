b3edd0776cf8186271cf67dab6f01fd88aeeb9fe
Zac Caffey <69201013+zaccaffey@users.noreply.github.com>
2021-11-30 18:40:17 -0600

    current

--- stage0main.C
+++ stage0main.C
@@ -1,11 +1,11 @@
-#include <stage1.h>
+#include <stage2.h>
 int main(int argc, char **argv)
 {
  // This program is the stage1 compiler for Pascallite. It will accept
  // input from argv[1], generate a listing to argv[2], and write object
  // code to argv[3].
  if (argc != 4) // Check to see if pgm was invoked correctly
-{
+ {
  // No; print error msg and terminate program
  cerr << "Usage: " << argv[0] << " SourceFileName ListingFileName "
  << "ObjectFileName" << endl;
